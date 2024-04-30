6166d8d9f0d0d3614f8778fb90e5aec93add7834
Lucas Santana <lucas.diassantana@gmail.com>
2024-04-30 18:27:25 -0300

    gambiarra test

--- functions/start/start.js
+++ functions/start/start.js
@@ -1,9 +1,9 @@
-const { startBot } = require("../../start.js");
+const startBot = require("../../start.js");
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    await startBot();
+    await startBot.startBot();
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `The bot has been started` }),
